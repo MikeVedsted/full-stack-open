d40de372b093e2dcfd451b9a70595cbbcbf0c215
Mike Vedsted <mikevedsted@gmail.com>
2021-02-09 10:34:52 +0200

    Add exercises ^4.2

---
+++ part-4/blogList/controllers/blogs.js
@@ -0,0 +1,60 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+
+// notesRouter.get('/:id', (request, response, next) => {
+//   Note.findById(request.params.id)
+//     .then(note => {
+//       if (note) {
+//         response.json(note)
+//       } else {
+//         response.status(404).end()
+//       }
+//     })
+//     .catch(error => next(error))
+// })
+
+
+// notesRouter.delete('/:id', (request, response, next) => {
+//   Note.findByIdAndRemove(request.params.id)
+//     .then(() => {
+//       response.status(204).end()
+//     })
+//     .catch(error => next(error))
+// })
+
+// notesRouter.put('/:id', (request, response, next) => {
+//   const body = request.body
+
+//   const note = {
+//     content: body.content,
+//     important: body.important,
+//   }
+
+//   Note.findByIdAndUpdate(request.params.id, note, { new: true })
+//     .then(updatedNote => {
+//       response.json(updatedNote)
+//     })
+//     .catch(error => next(error))
+// })
+
+module.exports = blogsRouter
